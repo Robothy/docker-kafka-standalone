ab74306737d2a69cb5c6692e96a4e0a9d9f686ce
Robothy <robothyluo@gmail.com>
2021-02-01 14:24:37 +0800

    increase delay

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
 $KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties &
-sleep 5s
+# sleep 15s to make sure zookeeper is started.
+sleep 15s
 $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties &
